b39c5debb1d65c7b47bd45bfeaaea31f7b683662
Jake Archibald <jaffathecake@gmail.com>
2014-05-01 11:25:46 +0100

    Working around Chrome bug for image fade ins (made object-fit fail)

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -1,6 +1,12 @@
 var flickrSearch = require('./flickr').search;
 var PhotosView = require('./views/photos');
 
+document.addEventListener('load', function(event) {
+  if (event.target.classList.contains('main-photo-img')) {
+    event.target.parentNode.classList.add('loaded');
+  }
+}, true);
+
 flickrSearch('train station').then(function(response) {
   document.querySelector('.photos').appendChild(new PhotosView(response).el);
 }).catch(function(err) {
